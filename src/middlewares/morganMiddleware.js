6e5b02c5ea9eec45f46b0920b32914533751f58a
sanjeet kumar <114237470+sanjeetkumar88@users.noreply.github.com>
2026-02-20 12:04:42 +0530

    added the logger

--- src/middlewares/morganMiddleware.js
+++ src/middlewares/morganMiddleware.js
@@ -6,16 +6,12 @@ const stream = {
   write: (message) => logger.http(message.trim()),
 };
 
-// Skip logging during tests or you can skip specific paths (like health checks)
-const skip = () => {
-  const env = process.env.NODE_ENV || "development";
-  return env !== "development";
-};
+
 
 // Build the morgan middleware
 const morganMiddleware = morgan(
   ":method :url :status :res[content-length] - :response-time ms",
-  { stream, skip }
+  { stream }
 );
 
 export default morganMiddleware;
